167d0c65d65b833bb09e0a811d8aea9abab27b15
Roman <xxxzadok@gmail.com>
2025-10-24 00:11:33 +0300

    Update admin ID

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv('8280338020:AAF89sCUSzDNLKf2dZ0AW_Swh7r764boY2Q')
-ADMIN_IDS = [356633485]  # Найди через @userinfobot
+BOT_TOKEN = os.getenv('8280338020:AAEPSrOAulipX1IYNGO_vppatAdsC0yc-t4')
+ADMIN_IDS = [356633485]  ## Замени на свой ID из @userinfobot
 WEBHOOK_URL = os.getenv('https://loyalty-telegram-bot-2igj.onrender.com')  # URL твоего Render сервиса
 
 # Конфигурация бота
